ceae60e664f959610190a4ef37d82eb65616b8fe
Tyler Hunt <tyhunt04@gmail.com>
2019-07-31 17:33:44 +0000

    Automatically backed up by Learn

--- src/Timer.js
+++ src/Timer.js
@@ -11,12 +11,17 @@ class Timer extends Component {
   }
 
   componentDidUpdate() {
-
+    this.timer.current.style.color =
+  "#" + Math.floor(Math.random() * 16777215).toString(16);
   }
 
-  shouldComponentUpdate() {
-
+  shouldComponentUpdate(nextProps, nextState) {
+    if (this.state.time === nextState.time) {
+      return false
+    }
+    return true
   }
+
 
   componentDidMount() {
     this.interval = setInterval(
